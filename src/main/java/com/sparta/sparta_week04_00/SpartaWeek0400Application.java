49c8af7aa2dfc18ad50471197ca954bc127db6a7
pmjn1025 <pmjn1025@naver.com>
2022-07-29 13:46:06 +0900

    기본 커밋

---
+++ src/main/java/com/sparta/sparta_week04_00/SpartaWeek0400Application.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_week04_00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaWeek0400Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaWeek0400Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/sparta_week04_00/SpartaWeek0400ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_week04_00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaWeek0400ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
